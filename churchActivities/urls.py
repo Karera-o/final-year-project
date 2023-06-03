1e08afd93e1a585d877e0f27f2ff10bb47d83c67
Karera-o <olivierkarera2020@gmail.com>
2023-06-03 23:39:33 +0200

    Finishing signup page

--- churchActivities/urls.py
+++ churchActivities/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.signUp, name='signUp'),
-    path('sign-in/', views.signIn, name='sign-in'),
+    path('', views.signUp, name='signup'),
+    path('signin/', views.signIn, name='signin'),
     path('admin-dashboard/', views.adminDashboard, name='admin-dashboard'),
     path('user-dashboard/', views.userDashboard, name='user-dashboard'),
 
--- churchActivities/views.py
+++ churchActivities/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def signUp(request):
+
+    pageTitle = 'SignUp'
+    content = {
+        'pageTitle': pageTitle,
+    }
     
-    return render(request, 'pages/signUp.html')
+    return render(request, 'pages/signUp.html',context)
 
 def signIn(request):
+
+    pageTitle = 'SignIn'
+    content = {
+        'pageTitle': pageTitle,
+    }
     
-    return render(request, 'pages/signIn.html') 
+    return render(request, 'pages/signIn.html',content) 
 
 def adminDashboard(request):
     
